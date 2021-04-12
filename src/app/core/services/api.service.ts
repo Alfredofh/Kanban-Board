57917ecf58dba99b31dfabbf4c3ae59577d4f6be
DESKTOP-06IB626\alfre <alfredofweb@gmail.com>
2021-04-12 15:40:59 +0200

    Create service and mock data

---
+++ src/app/core/services/api.service.ts
@@ -0,0 +1,31 @@
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+import { throwError as observableThrowError } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  //api with one task
+  //private apiRoot: string = 'https://run.mocky.io/v3/26045374-863c-469d-85c4-51ea1135ce8a';
+  //api without any task
+  private apiRoot: string = 'https://run.mocky.io/v3/7841d1af-e8d5-446a-bac5-3506fdd05659';
+  // api with many task
+  //private apiRoot: string = 'https://run.mocky.io/v3/0933ddef-c9bf-4f26-8ddf-77990fb490cb';
+
+
+  getApi() {
+    return this.http
+      .get<Array<{}>>(this.apiRoot)
+      .pipe(map(data => data), catchError(this.handleError));
+  }
+
+  private handleError(res: HttpErrorResponse) {
+    return observableThrowError(res.error || 'Server error');
+  }
+
+  constructor(private http: HttpClient) { }
+}
